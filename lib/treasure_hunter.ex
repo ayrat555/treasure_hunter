3cce2bd519ebec0790b950a35508e18397f6927c
Ayrat Badykov <ayratin555@gmail.com>
2022-03-17 20:24:00 +0200

    initial commit

---
+++ lib/treasure_hunter.ex
@@ -0,0 +1,18 @@
+defmodule TreasureHunter do
+  @moduledoc """
+  Documentation for `TreasureHunter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TreasureHunter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/treasure_hunter_test.exs
@@ -0,0 +1,8 @@
+defmodule TreasureHunterTest do
+  use ExUnit.Case
+  doctest TreasureHunter
+
+  test "greets the world" do
+    assert TreasureHunter.hello() == :world
+  end
+end
